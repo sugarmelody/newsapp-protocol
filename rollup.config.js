d6cf41dd2a195d4551b98c070c733f54e1c46e05
sugarmelody <sugarmelody@163.com>
2017-06-27 11:57:22 +0800

    umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
 import babel from 'rollup-plugin-babel'
-import eslint from 'rollup-plugin-eslint'
+//import eslint from 'rollup-plugin-eslint'
 
 export default {
   entry: 'lib/index.js',
   dest: 'dist/newsapp.js', // 输出文件
-  format: 'cjs',
-  sourceMap: process.env.NODE_ENV === 'production' ? false : 'inline',
+  targets: [
+    {
+      format: 'iife',
+      //format: 'umd',
+      moduleName: 'NEWSAPPAPI',
+      sourceMap: process.env.NODE_ENV === 'production' ? false : 'inline',
+    }
+  ],
   plugins: [
     babel({
       exclude: ['node_modules/**', 'dist/**']
