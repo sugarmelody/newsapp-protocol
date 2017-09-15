efd0561cb20213c13767780271574ae2b81bdf50
sugarmelody <sugarmelody@163.com>
2017-09-15 15:00:04 +0800

    comment

--- devServer.js
+++ devServer.js
@@ -11,3 +11,34 @@ app.use(express.static(__dirname + '/demo'))
 const server = app.listen(3200, function () {
   console.log('listening at http://localhost:3200/')
 });
+
+const assert = require('assert');
+
+/*const obj1 = {
+  a: {
+    b: {
+      c: {
+        d:1
+      }
+    }
+  }
+};
+const obj2 = {
+  a: {
+    b: 2
+  }
+};
+const obj3 = {
+  a: {
+    b: {
+      c: {
+        d:1
+      }
+    }
+  }
+};
+const obj4 = Object.create(obj1);*/
+
+
+// assert.deepEqual(obj1, obj3);
+// 通过，两个对象相等
